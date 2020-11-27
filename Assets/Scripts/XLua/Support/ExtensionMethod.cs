ae92db2525f56a72e0e00f6da0c062d73b581a18
gggg826 <liushida87@126.com>
2020-11-27 22:54:55 +0800

    add ExtensionMethod

---
+++ Assets/Scripts/XLua/Support/ExtensionMethod.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ExtensionMethod
+{
+    public static bool Remove(this Dictionary<string, UnityEngine.GameObject> dic, string key, out GameObject value)
+    {
+        if(dic == null || !dic.ContainsKey(key))
+        {
+            value = null;
+            return false;
+        }
+        else
+        {
+            value = dic[key];
+            dic.Remove(key);
+            return true;
+        }
+    }
+
+    public static bool TryAdd(this Dictionary<string, UnityEngine.GameObject> dic, string key, GameObject value)
+    {
+        if (dic == null || dic.ContainsKey(key))
+        {
+            return false;
+        }
+        else
+        {
+            dic.Add(key, value);
+            return true;
+        }
+    }
+}
